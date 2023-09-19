baa91c8460d2e1da9837aef8b8495a14f98fe68c
terr0001 <oohpier@gmail.com>
2023-09-19 10:51:03 +1000

    upload initial files

---
+++ CheckRay/CheckRay/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CheckRay.Startup))]
+namespace CheckRay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
