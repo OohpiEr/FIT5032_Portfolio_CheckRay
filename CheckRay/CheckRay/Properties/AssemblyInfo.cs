64f2c3b1af3f45f8e95aeb866c6b1b30923a5add
terr0001 <oohpier@gmail.com>
2023-10-11 20:21:58 +1100

    Created new app for code first approach

--- CheckRay/CheckRay/Properties/AssemblyInfo.cs
+++ CheckRay/CheckRay/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3b5d0a19-df55-4cb7-897b-1b78e3e156e9")]
+[assembly: Guid("5f7ddf9a-605f-4557-977c-403ea56530d9")]
 
 // Version information for an assembly consists of the following four values:
 //
