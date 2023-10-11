5c024e4df33f119b5ee5cef83ab55894a3f23513
terr0001 <oohpier@gmail.com>
2023-10-11 20:22:34 +1100

    Added domain classes skeleton

---
+++ CheckRay/CheckRay/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CheckRay.Models
+{
+    public class User
+    {
+        public int id { get; }
+        public string email { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int role { get; set; }
+
+        enum Role : int
+        {
+            ADMIN = 1,
+            PATIENT = 2,
+            DOCTOR = 3
+        }
+
+    }
+    
+}
